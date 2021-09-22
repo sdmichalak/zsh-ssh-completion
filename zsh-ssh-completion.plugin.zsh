758fc144c4be1232dcf819f9de2c6f7508fbb400
Sebastian Michalak <sebastian.michalak@forcom.com.pl>
2021-09-22 23:22:39 +0200

    initial commit

--- zsh-ssh-completion.plugin.zsh
+++ zsh-ssh-completion.plugin.zsh
@@ -3,7 +3,7 @@
 
 h=()
 if [[ -r ~/.ssh/config ]]; then
-  h=($h ${${${(@M)${(f)"$(cat ~/.ssh/config)"}:#Host *}#Host }:#*[*?]*})
+  h=($h ${${${(@M)${(f)"$(cat ~/.ssh/config ~/.ssh/config.d/*.sshconfig)"}:#Host *}#Host }:#*[*?]*})
 fi
 if [[ -r ~/.ssh/known_hosts ]]; then
   h=($h ${${${(f)"$(cat ~/.ssh/known_hosts{,2} || true)"}%%\ *}%%,*}) 2>/dev/null
