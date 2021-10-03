480b11a29b329c4d00fd99ce7c910c240007668d
Sebastian Michalak <sebastian.michalak@forcom.com.pl>
2021-10-03 14:00:49 +0200

    Added newlines

--- zsh-ssh-completion.plugin.zsh
+++ zsh-ssh-completion.plugin.zsh
@@ -1,15 +1,16 @@
 #!/usr/bin/env zsh
-# File based on https://github.com/zpm-zsh/ssh
 
 h=()
+
 if [[ -r ~/.ssh/config ]]; then
   h=($h ${${${(@M)${(f)"$(cat ~/.ssh/config ~/.ssh/config.d/*.sshconfig)"}:#Host *}#Host }:#*[*?]*})
 fi
+
 if [[ -r ~/.ssh/known_hosts ]]; then
   h=($h ${${${(f)"$(cat ~/.ssh/known_hosts{,2} || true)"}%%\ *}%%,*}) 2>/dev/null
 fi
+
 if [[ $#h -gt 0 ]]; then
   zstyle ':completion:*:ssh:*' hosts $h
   zstyle ':completion:*:slogin:*' hosts $h
 fi
-
